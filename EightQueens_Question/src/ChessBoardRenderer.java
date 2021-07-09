9217b3de0665faf6027d3990cc4427c846a30ed9
Santhan <Santhan@MSI>
2021-07-09 12:10:58 +0530

    8 queens solution

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -3,6 +3,14 @@ public class ChessBoardRenderer {
 
 		// WRITE YOUR LOGIC HERE...
 		System.out.println(square);
-		return true;
+		int c = square/8;
+		int r = square%8;
+		if(c%2==0 && r%2!=0) {
+			return true;
+		}
+		if(c%2!=0 && r%2==0) {
+			return true;
+		}
+		return false;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,14 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		int c = cellId/8;
+		return c;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		int r = cellId%8;
+		return r;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -27,8 +27,62 @@ public class GameStrategy {
 		
 		/*
 			WRITE YOUR LOGIC HERE...............................
-
+			
 		*/
+		for(int i=0;i<8;i++) {
+			if(placedQueens[row][i]==true) {
+				isValid = false;
+			}
+		}
+		for(int i=0;i<8;i++) {
+			if(placedQueens[i][col]==true) {
+				isValid = false;
+			}
+		}
+		int i = row;
+		int j = col;
+		while(i<8 && j<8) {
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+			}
+			i = i+1;
+			j = j+1;
+			
+		}
+		i = row;
+		j = col;
+		while(i<8 && j>=0) {
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+			}
+			i = i+1;
+			j = j-1;
+			
+		}
+		i = row;
+		j = col;
+		while(i>=0 && j<8) {
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+			}
+			i = i-1;
+			j = j+1;
+			
+		}
+		i = row;
+		j = col;
+		while(i>=0 && j>=0) {
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+			}
+			i = i-1;
+			j = j-1;
+			
+		}
+		if (isValid==true) {
+			placedQueens[row][col] = true;
+			numQueens = numQueens+1;
+		}
 		return isValid;
 	}
 }
